b8844dd2e80910b25bf4044d4eaf00598eb41773
VarshaRajput12 <varsharajput353@gmail.com>
2023-02-11 02:09:33 +0530

    word game

---
+++ scripts/array.js
@@ -0,0 +1,238 @@
+const wordList = [
+  {
+    word: "python",
+    hint: "Programming language",
+  },
+  {
+    word: "guitar",
+    hint: "A musical instrument",
+  },
+  {
+    word: "aim",
+    hint: "A purpose or intension",
+  },
+  {
+    word: "earth",
+    hint: "The planet on which life exist",
+  },
+  {
+    word: "gold",
+    hint: "A yellow precious metal",
+  },
+  {
+    word: "idea",
+    hint: "A thought or suggestion",
+  },
+  {
+    word: "search",
+    hint: "Act to find something",
+  },
+  {
+    word: "coding",
+    hint: "Related to programming",
+  },
+  {
+    word: "map",
+    hint: "Diagram representation of area",
+  },
+  {
+    word: "island",
+    hint: "Land surrounded by water",
+  },
+  {
+    word: "hockey",
+    hint: "National game of india",
+  },
+  {
+    word: "globe",
+    hint: "Modal of the Earth",
+  },
+  {
+    word: "bird",
+    hint: "I love to chirp",
+  },
+  {
+    word: "cake",
+    hint: "I am served at birthday parties",
+  },
+  {
+    word: "bed",
+    hint: "I am good to sleep in",
+  },
+  {
+    word: "television",
+    hint: "People sit and watch me",
+  },
+  {
+    word: "table",
+    hint: "It has four legs made of wood ",
+  },
+  {
+    word: "home",
+    hint: "We all live here",
+  },
+  {
+    word: "snowman",
+    hint: "Made of snow",
+  },
+  {
+    word: "chair",
+    hint: "People sit on me",
+  },
+  {
+    word: "diver",
+    hint: "Person who ride vehicles",
+  },
+  {
+    word: "car",
+    hint: "I have four wheels",
+  },
+  {
+    word: "monkey",
+    hint: "I can swing one tree to another",
+  },
+  {
+    word: "rocket",
+    hint: "Using it we can go to the moon",
+  },
+  {
+    word: "rainbow",
+    hint: "Sometimes it comes after rain",
+  },
+  {
+    word: "butterfly",
+    hint: "Insect that collect nectar",
+  },
+  {
+    word: "flowers",
+    hint: "They smell very good",
+  },
+  {
+    word: "mumbai",
+    hint: "City which known as film city",
+  },
+  {
+    word: "china",
+    hint: "Largest populated country",
+  },
+  {
+    word: "pacific",
+    hint: "Lagest ocean in the world",
+  },
+  {
+    word: "holi",
+    hint: "Festival of colours",
+  },
+  {
+    word: "diwali",
+    hint: "This day lord Ram returned to ayodhya ",
+  },
+  {
+    word: "camel",
+    hint: "Ship of the desert",
+  },
+  {
+    word: "tadpole",
+    hint: "Baby frog",
+  },
+  {
+    word: "five",
+    hint: "Number of vowels",
+  },
+  {
+    word: "five",
+    hint: "Number of sense orgens",
+  },
+  {
+    word: "eyes",
+    hint: "Help us to see",
+  },
+  {
+    word: "ears",
+    hint: "Help us to hear",
+  },
+  {
+    word: "tongue",
+    hint: "Tell us the taste of food ",
+  },
+  {
+    word: "jupiter",
+    hint: "Largest planet of our solar system",
+  },
+  {
+    word: "sun",
+    hint: "Main source of energy for the Earth",
+  },
+  {
+    word: "mercury",
+    hint: "The planet nearest to the Earth",
+  },
+  {
+    word: "ostrich",
+    hint: "Bird that lays the largest eggs",
+  },
+  {
+    word: "mars",
+    hint: "The planet known as the Red Planet",
+  },
+  {
+    word: "moon",
+    hint: "Natural satellite of Earth",
+  },
+  {
+    word: "lion",
+    hint: "Known as the king of the jungle",
+  },
+  {
+    word: "peacock",
+    hint: "Bird that dances in the rain",
+  },
+  {
+    word: "amazon",
+    hint: "Largest forest in the world",
+  },
+  {
+    word: "yamuna",
+    hint: "Delhi is situated on the bank of this river",
+  },
+  {
+    word: "bihu",
+    hint: "Folk dance of Assam",
+  },
+  {
+    word: "jaipur",
+    hint: "City known as pink city",
+  },
+  {
+    word: "cactus",
+    hint: "Plant grow in the desert",
+  },
+  {
+    word: "gujarat",
+    hint: "Gir National Forest located in ?",
+  },
+  {
+    word: "calf",
+    hint: "Young one of a cow",
+  },
+  {
+    word: "equal",
+    hint: "All sides of a square are ?",
+  },
+  {
+    word: "ludo",
+    hint: "An indoor game",
+  },
+  {
+    word: "punjab",
+    hint: "Giddha is the folk dance of",
+  },
+  {
+    word: "nile",
+    hint: "The longest river on the earth",
+  },
+  {
+    word: "cricket",
+    hint: "The game which is played with bat, ball",
+  },
+];
---
+++ scripts/script.js
@@ -0,0 +1,74 @@
+const inputs = document.querySelector(".inputs");
+const resetBtn = document.querySelector(".reset_btn");
+const hint = document.querySelector(".hint span");
+const guessLeft = document.querySelector(".guess_left span");
+const wrongLetters = document.querySelector(".wrong_letters span");
+const typingInput = document.querySelector(".typing_input");
+let word,
+  maxGuesses,
+  correct = [],
+  incorrect = [];
+function randomWord() {
+  let ranObj = wordList[Math.floor(Math.random() * wordList.length)];
+  word = ranObj.word;
+//   console.log(word);
+  maxGuesses = 8;
+  correct = [];
+  incorrect = [];
+  hint.innerText = ranObj.hint;
+  guessLeft.innerText = maxGuesses;
+  wrongLetters.innerText = incorrect;
+  let html = "";
+  for (let i = 0; i < word.length; i++) {
+    html += `<input type="text" disabled>`;
+  }
+  inputs.innerHTML = html;
+}
+randomWord();
+
+resetBtn.addEventListener("click", randomWord);
+
+function initGame(e) {
+  let key = e.target.value;
+  if (
+    key.match(/^[A-Za-z]+$/) &&
+    !incorrect.includes(`  ${key.toUpperCase()}`) &&
+    !correct.includes(key)
+  ) {
+    if (word.includes(key)) {
+      // console.log("found")
+      for (let i = 0; i < word.length; i++) {
+        if (word[i] === key) {
+          correct.push(key);
+          inputs.querySelectorAll("input")[i].value = key;
+        }
+      }
+    } else {
+      // console.log("not found")
+      maxGuesses--;
+      incorrect.push(`  ${key}`);
+    }
+    guessLeft.innerText = maxGuesses;
+    wrongLetters.innerText = incorrect;
+  }
+  typingInput.value = "";
+  setTimeout(()=>{
+    if (correct.length === word.length) {
+      alert(`Congrat🎊🎉! you found the word "${word.toUpperCase()}"`);
+      randomWord();
+    } else if (maxGuesses === 0) {
+      alert("Game over! You don't have remaining guesses");
+      for (let i = 0; i < word.length; i++) {
+        inputs.querySelectorAll("input")[i].value = word[i];
+      }
+    }
+  })
+}
+
+typingInput.addEventListener("input", initGame);
+// inputs.addEventListener("click", () => {
+//   typingInput.focus();
+// });
+document.addEventListener("keydown", () => {
+  typingInput.focus();
+});
